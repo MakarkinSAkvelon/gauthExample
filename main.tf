de10b72f5802c5338f088689e57926483d988865
MakarkinSAkvelon <67736809+MakarkinSAkvelon@users.noreply.github.com>
2022-05-23 15:58:20 +0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  credentials = ""
+  project = "playgroundsandbox"
+  region = "us-central1"
+  zone = "us-central1-a"
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "last-tst"
+    prefix  = "terraform/state"
+  }
+}
+
+resource "google_compute_instance" "mytst" {
+  name = "mytst"
+  machine_type = "e2-micro"
+  boot_disk {
+    initialize_params {
+        image = "ubuntu-2004-focal-v20220419"
+    }
+  }
+
+
+ network_interface {
+    network = "default"
+}
+}
